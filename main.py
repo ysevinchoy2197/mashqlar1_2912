34f47710f430e9e8001bbecc392a5bcce4ebe493
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-12-29 13:28:55 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#1-misol
+import math
+
+son = float(input("Son kiriting: "))
+natija = math.sqrt(son)
+
+print("Kvadrat ildizi:", natija)
+
+#2-misol
+import math
+
+a = float(input("Son kiriting: "))
+natija = math.pow(a, 3)
+
+print("Kubi:", natija)
+
+
+#3-misol
+import math
+
+a = float(input("Sonning modulini toping: "))
+natija = math.fabs(a)
+
+print("moduli:", natija)
+
+
+#4-misol
+import math
+
+a = float(input("Burchakni gradusda kiriting: "))
+natija = math.radians(a), math.sin(a)
+
+print("moduli:", natija)
